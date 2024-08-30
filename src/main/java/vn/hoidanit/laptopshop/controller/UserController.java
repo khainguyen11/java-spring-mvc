1fe3e37cc4e11fa6c72ef744106fe7b565e693d8
khainguyen11 <KhaiNHH.B21VT243@stu.ptit.edu.vn>
2024-08-31 01:10:56 +0700

    push

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,37 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+//MVc
+// @Controller
+// public class UserController {
+
+//     @RequestMapping("/")
+//     public String getHomePage() {
+//         return "Hello from controller";
+//     }
+// }
+//Restful Api
+@RestController
+
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    public String getHomePage() {
+        return this.userService.handleHello();
+    }
+
+}
